3da524643a49a465e068aceaf9e9dc1f3ef66461
Ali Izzaldin <ali.izzaldin7@gmail.com>
2024-02-20 10:53:27 +0300

    finally completed

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+// scuberGreetingForFeet function
+function scuberGreetingForFeet(distance) {
+  if (distance <= 400) {
+    return 'This one is on me!';
+  
+  } else if(distance >= 400 && distance <= 2000){
+    return 'That will be twenty bucks.';
+  }
+  else if (distance > 2000 && distance <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } else {
+    return 'Please provide a valid distance.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+// ternaryCheckCity function
+function ternaryCheckCity(destination) {
+  return destination === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+// switchOnCharmFromTip function
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+      return 'Bye.';
+  }
 }
+
+console.log(switchOnCharmFromTip(7))
